3ff4a1af121f05ec207fce389ca46f993a7b6e8a
Davide Bettio <davide@uninstall.it>
2018-07-20 01:40:27 +0200

    shell: add simple keyboard demo
    Add simple demo that allows to move a square using keyboard.

--- components/shell/shell_main.c
+++ components/shell/shell_main.c
@@ -5,8 +5,10 @@
 
 void shell_main(void *hwcontext)
 {
-    painter_draw_rect(hwcontext, 5, 5, 74, 38, PAINTER_BLACK);
     hwcontext_update_screen(hwcontext);
+
+    int x_pos = 0;
+    int y_pos = 0;
 
     int c;
     int pressed;
@@ -14,5 +16,26 @@ void shell_main(void *hwcontext)
     do {
         c = hwcontext_get_key_code(hwcontext, &pressed, &ts);
         printf("last char: %c pressed: %d timestamp: secs %ld nsecs %ld\n", c, pressed, ts.tv_sec, ts.tv_nsec);
+
+        painter_clear_screen(hwcontext);
+        painter_draw_rect(hwcontext, 5, 5, 74, 38, PAINTER_BLACK);
+        if (pressed) {
+            switch (c) {
+                case '0':
+                    x_pos += 1;
+                    break;
+                case '1':
+                    x_pos -= 1;
+                    break;
+                case '2':
+                    y_pos += 1;
+                    break;
+                case '3':
+                    y_pos += 1;
+                    break;
+            }
+        }
+        painter_draw_rect(hwcontext, x_pos, y_pos, 3, 3, PAINTER_BLACK);
+
     } while (c != 'C');
 }
