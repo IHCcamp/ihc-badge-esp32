8314ccfd39de9f0dfb24baa6acdfc93cc3d71ab5
Davide Bettio <davide@uninstall.it>
2018-07-19 14:39:35 +0200

    shell: paint test horizontal line
    Test framebuffer.

--- components/shell/shell_main.c
+++ components/shell/shell_main.c
@@ -1,9 +1,12 @@
 #include "hwcontext.h"
+#include "painter.h"
 
 #include <stdio.h>
 
 void shell_main(void *hwcontext)
 {
+    painter_draw_h_line(hwcontext, 2, 4, 30, PAINTER_BLACK);
+
     int c;
     do {
         c = hwcontext_get_key_code(hwcontext);
