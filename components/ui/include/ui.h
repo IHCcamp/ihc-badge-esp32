70e20b99a91726c25dd7b1846827209aff2a5dac
Davide Bettio <davide@uninstall.it>
2018-07-21 15:05:14 +0200

    ui: add show_menu
    Add function that waits for a user choice

--- components/ui/include/ui.h
+++ components/ui/include/ui.h
@@ -2,5 +2,6 @@
 #define _UI_H_
 
 void ui_show_message(void *hwcontext, const char *message);
+int ui_show_menu(void *hwcontext, int num_entries, const char * const *entries, int current_entry);
 
 #endif
--- components/ui/ui.c
+++ components/ui/ui.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <time.h>
 
+#define ROW_HEIGHT 8
+
+static void display_menu(void *hwcontext, int num_entries, const char * const *entries, int current_entry);
 static void wait_menu_key_press(void *hwcontext);
 
 void ui_show_message(void *hwcontext, const char *message)
@@ -16,6 +19,47 @@ void ui_show_message(void *hwcontext, const char *message)
     hwcontext_update_screen(hwcontext);
 
     wait_menu_key_press(hwcontext);
+}
+
+int ui_show_menu(void *hwcontext, int num_entries, const char * const *entries, int current_entry)
+{
+    char c;
+    int pressed;
+
+    do {
+        painter_clear_screen(hwcontext);
+        display_menu(hwcontext, num_entries, entries, current_entry);
+
+        struct timespec ts;
+        c = hwcontext_get_key_code(hwcontext, &pressed, &ts);
+
+        if (!pressed && (c == 'U') && (current_entry > 0)) {
+            current_entry--;
+        } else if (!pressed && (c == 'D') && (current_entry < num_entries - 1)) {
+            current_entry++;
+        }
+    } while ((c != 'M') || !pressed);
+
+    return current_entry;
+}
+
+static void display_menu(void *hwcontext, int num_entries, const char * const *entries, int current_entry)
+{
+    int first_visible_entry = (current_entry / 6) * 6;
+    int current_visible_entry = current_entry % 6;
+    int visible_rows = (num_entries - first_visible_entry > 6) ? 6 : num_entries - first_visible_entry;
+
+    for (int i = 0; i < visible_rows; i++) {
+        int color = PAINTER_BLACK;
+
+        if (i == current_visible_entry) {
+            painter_draw_fill_rect(hwcontext, 0, i * ROW_HEIGHT, PAINTER_SCREEN_WIDTH, ROW_HEIGHT - 1, PAINTER_BLACK);
+            color = PAINTER_WHITE;
+        }
+
+        painter_draw_text(hwcontext, 1, i, entries[first_visible_entry + i], PAINTER_FONT_REGULAR, color);
+    }
+    hwcontext_update_screen(hwcontext);
 }
 
 static void wait_menu_key_press(void *hwcontext)
