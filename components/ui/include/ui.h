a2100e92e50597b18c1bfe5ee5a444e63b5ec23c
carlo maragno <ste.maragno@gmail.com>
2018-07-21 17:11:53 +0200

    shell_main: add a demo for the animation function
    ui_draw_animation: add frames count inside xbm_merger output file
    
    shell_main: fix whitespace
    
    shell_main: improved startup sequence
    
    shell_main: fix whitespaces
    
    fix spelling

--- components/ui/include/ui.h
+++ components/ui/include/ui.h
@@ -6,6 +6,4 @@ int ui_show_menu(void *hwcontext, int num_entries, const char * const *entries, 
 char *ui_ask_user_input(void *hwcontext, const char *message);
 void ui_draw_animation(void *hwcontext, int framescount, int fps, const unsigned char *img_bits, int x, int y, int width, int height);
 
-
-
 #endif
