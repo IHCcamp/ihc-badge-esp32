85bef0954637db20a75a5e9d30928b7bd802cc01
carlo maragno <ste.maragno@gmail.com>
2018-07-21 17:10:42 +0200

    ui: add ui_darw_animation
    ui_draw_animation: fix typos
    
    ui_draw_animation: fxi typo

--- components/ui/include/ui.h
+++ components/ui/include/ui.h
@@ -4,5 +4,8 @@
 void ui_show_message(void *hwcontext, const char *message);
 int ui_show_menu(void *hwcontext, int num_entries, const char * const *entries, int current_entry);
 char *ui_ask_user_input(void *hwcontext, const char *message);
+void ui_draw_animation(void *hwcontext, int framescount, int fps, const unsigned char *img_bits, int x, int y, int width, int height);
+
+
 
 #endif
