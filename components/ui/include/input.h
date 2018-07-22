5611e3f628b4a6629000182db2c1b794b59f974f
Riccardo Binetti <rbino@gmx.com>
2018-07-22 15:35:10 +0200

    Add input.h/input.c
    Implement input_consume_key_event

---
+++ components/ui/include/input.h
@@ -0,0 +1,22 @@
+#ifndef _INPUT_H_
+#define _INPUT_H_
+
+#include <time.h>
+
+#define EDITING_BUFFER_LEN 128
+struct EditingState
+{
+    int last_char;
+    char buffer[EDITING_BUFFER_LEN];
+    int upper_case;
+    char last_key;
+    int last_pressed_state;
+    struct timespec last_timestamp;
+    int choices_length;
+    int choice_idx;
+    const char *char_choices;
+};
+
+void input_consume_key_event(struct EditingState *state, char key, int pressed, const struct timespec *ts);
+
+#endif
