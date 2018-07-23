bac225b0c0c2e944c6993f0c6e5c5775f0951cc7
Riccardo Binetti <rbino@gmx.com>
2018-07-23 20:45:02 +0200

    ui: add ui_print_menu_button_label

--- components/ui/include/ui.h
+++ components/ui/include/ui.h
@@ -3,6 +3,7 @@
 
 void ui_show_message(void *hwcontext, const char *message);
 int ui_show_menu(void *hwcontext, int num_entries, const char * const *entries, int current_entry);
+void ui_print_menu_button_label(void *hwcontext, const char *label);
 char *ui_ask_user_input(void *hwcontext, const char *message);
 void ui_draw_animation(void *hwcontext, int framescount, int fps, const unsigned char *img_bits, int x, int y, int width, int height);
 
