2184a5fa33d03f8b3f90eb82ab4e2e3ca1ca031c
Davide Bettio <davide@uninstall.it>
2018-07-23 21:30:48 +0200

    settings: add demo code
    Add simple demo list menu.

--- components/settings_app/settings_main.c
+++ components/settings_app/settings_main.c
@@ -9,5 +9,21 @@
 
 void settings_main(struct AppContext *appctx)
 {
-    printf("Henlo World\n");
+    void *hwcontext = appctx->hwcontext;
+
+    const char *const choices[] = {
+        "Booting",
+        "Lisa Frank 420",
+        "Flower Shoppe",
+        "Library",
+        "Geography",
+        "Chill Divin' with ECCO",
+        "Mathematics",
+        "Foreign Banks Av...",
+        "Te",
+        "Moon",
+        "Seabed"
+    };
+
+    ui_show_menu(hwcontext, 11, choices, 2);
 }
