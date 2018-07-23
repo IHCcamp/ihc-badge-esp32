ddf5934ce195e4fe0852a2f0ddb9411fe4434748
Davide Bettio <davide@uninstall.it>
2018-07-24 00:30:41 +0200

    Add interpreter scaffold
    Add all needed application scaffolding.

--- components/shell/apps/app_entry_points.h
+++ components/shell/apps/app_entry_points.h
@@ -1,6 +1,7 @@
 #ifndef _APP_ENTRY_POINTS_H_
 #define _APP_ENTRY_POINTS_H_
 
+void interp_main(struct AppContext *appctx);
 void settings_main(struct AppContext *appctx);
 
 #endif
--- components/shell/apps/apps_list.c
+++ components/shell/apps/apps_list.c
@@ -5,10 +5,12 @@
 #include "app_entry_points.h"
 
 #include "settings.xbm"
+#include "interp.xbm"
 
 const struct MenuEntry menu_entries[] =
 {
     {"Settings", settings_bits, settings_main},
+    {"Interp.", interp_bits, interp_main},
     {NULL, NULL, NULL}
 };
 
