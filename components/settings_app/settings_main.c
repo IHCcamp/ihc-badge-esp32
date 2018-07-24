e76f3108e870e61662efa8479ccb7f251d6f56a0
Riccardo Binetti <rbino@gmx.com>
2018-07-24 15:13:40 +0200

    settings_main: implement set name

--- components/settings_app/settings_main.c
+++ components/settings_app/settings_main.c
@@ -7,23 +7,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CHOICES_ENTRIES 1
+
+#define SET_NAME_ENTRY 0
+
+static void set_name(struct AppContext *appctx);
+
 void settings_main(struct AppContext *appctx)
 {
     void *hwcontext = appctx->hwcontext;
 
     const char *const choices[] = {
-        "Booting",
-        "Lisa Frank 420",
-        "Flower Shoppe",
-        "Library",
-        "Geography",
-        "Chill Divin' with ECCO",
-        "Mathematics",
-        "Foreign Banks Av...",
-        "Te",
-        "Moon",
-        "Seabed"
+        "Set name"
     };
 
-    ui_show_menu(hwcontext, 11, choices, 2);
+    int choice;
+    do {
+        choice = ui_show_menu(hwcontext, CHOICES_ENTRIES, choices, 0);
+        switch(choice) {
+            case SET_NAME_ENTRY:
+                set_name(appctx);
+                break;
+        }
+    } while (choice >= 0);
+}
+
+static void set_name(struct AppContext *appctx)
+{
+    void *hwcontext = appctx->hwcontext;
+
+    char *new_name = ui_ask_user_input(hwcontext, "Insert name");
+    if (new_name) {
+        free(appctx->user_name);
+        appctx->user_name = new_name;
+    }
 }
